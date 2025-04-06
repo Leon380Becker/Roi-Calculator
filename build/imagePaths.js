e103ee04509318cf82d20f622a6b42c59f9177f7
Leon380Becker <leonbecker380@gmail.com>
2025-04-06 01:50:50 +0000

    Add files via upload

---
+++ build/imagePaths.js
@@ -0,0 +1,13 @@
+export const IMAGES = {
+  EaCalculator: "../img/EA_calculator_graphic.svg",
+  EaLogoHorizontalBlack: "../img/EA_Logo_Horizontal_RGB_Black.svg",
+  EaLogoHorizontalViolet: "../img/EA_Logo_Horizontal_RGB_Deep_Violet.svg",
+  EaLogoHorizontalWhite: "../img/EA_Logo_Horizontal_RGB_White.svg",
+  EaLogoBlack: "../img/EA_Logo_Stacked_RGB_Black.svg",
+  EaLogoViolet: "../img/EA_Logo_Stacked_RGB_Deep_Violet.svg",
+  EaLogoWhite: "../img/EA_Logo_Stacked_RGB_White.svg",
+  ProgramsIcon: "../img/Programs_Icon.svg",
+  RoadmapsIcon: "../img/Roadmaps_Icon.svg",
+  TeamRhythmIcon: "../img/TeamRhythm_Icon.svg",
+}
+
